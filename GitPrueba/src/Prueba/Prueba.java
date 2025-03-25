887a141c906be4974eb633072ede22045d6a16f2
Ivan Kutsenko <programacion@programacion-VirtualBox>
2025-03-25 21:08:41 +0100

    ivan

--- GitPrueba/src/Prueba/Prueba.java
+++
@@ -1,7 +0,0 @@
-package Prueba;
-
-public class Prueba {
-	public static void main(String [] args) {
-		System.out.println("Version 1");
-	}
-}
---
+++ GitPrueba/src/datosIvanKutsenkoo/datosIvánKutsenkoo.java
@@ -0,0 +1,7 @@
+package datosIvanKutsenkoo;
+
+public class datosIvánKutsenkoo{
+	public static void main(String [] args) {
+		System.out.println("Iván Kutsenko");
+	}
+}
