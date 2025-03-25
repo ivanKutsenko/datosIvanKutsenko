b2b5b174c17d522be6397da78b79ae436bf90d3d
Ivan Kutsenko <programacion@programacion-VirtualBox>
2025-03-25 21:10:25 +0100

    edad

--- GitPrueba/src/datosIvanKutsenkoo/datosIvánKutsenkoo.java
+++ GitPrueba/src/datosIvanKutsenkoo/datosIvánKutsenkoo.java
@@ -3,5 +3,6 @@ package datosIvanKutsenkoo;
 public class datosIvánKutsenkoo{
 	public static void main(String [] args) {
 		System.out.println("Iván Kutsenko");
+		System.out.println("Tengo19 años");
 	}
 }
